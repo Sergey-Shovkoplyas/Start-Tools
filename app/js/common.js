3ef066f0fff4d164821222d4bfc0af09db9c5915
Sergey Shovkoplyas <shovkoplyassergey@gmail.com>
2019-07-09 02:38:22 +0300

    add sections

--- app/js/common.js
+++ app/js/common.js
@@ -1,16 +1,8 @@
-/* window.onload = function () {
-	
-	console.log('js is working!!!'); 
-
-} */
-
 $( document ).ready(function() {
 
 	// show catalog
-
 	let $catalog = $('.catalog');
 	let $catalogMenu = $('.catalog__menu');
-	
 	$catalog.hover(
 		function(){
 			$catalogMenu.slideDown();
@@ -19,12 +11,10 @@ $( document ).ready(function() {
 			$catalogMenu.slideUp();
 		}
 	);
-
-	// show catalog__submenu
-
-	let $catalogCategory = $('.catalog__menu li');
 	
 
+	// show catalog__submenu
+	let $catalogCategory = $('.catalog__menu li');
 	$catalogCategory.hover(
 		function(){
 			$(this).find('.catalog__submenu-wrap').fadeIn(100);
@@ -33,5 +23,60 @@ $( document ).ready(function() {
 			$(this).find('.catalog__submenu-wrap').fadeOut(100);
 		}
 	);
+
+	// initialize sales__slider
+	$('.sales__slider').slick({
+		slidesToShow: 4,
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: {
+					slidesToShow: 3
+				}
+			},
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 2
+				}
+			},
+			{
+				breakpoint: 580,
+				settings: {
+					slidesToShow: 1,
+					arrows: false
+				}
+			}
+		]
+	});
+
+	// initialize video__slider
+	$('.video__slider').slick({
+		slidesToShow: 4,
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: {
+					slidesToShow: 3
+				}
+			},
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 2
+				}
+			},
+			{
+				breakpoint: 580,
+				settings: {
+					slidesToShow: 1,
+					arrows: false
+				}
+			}
+		]
+	});
+
+	// initialize banner__slider
+	$('.banner__slider').slick();
 
 });
